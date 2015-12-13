3acadf61c5e66dd141cee7a5dc5939a7b4a46071
Brian Weidenbaum <brianweiden@gmail.com>
2015-12-12 17:31:39 -0800

    more cleanup

--- script.js
+++ script.js
@@ -1,59 +1,59 @@
 (function() {
-var SHOWING = 'showing';
+  var SHOWING = 'showing';
 
-var blog = document.getElementById('blog');
-var home = document.getElementById('home');
+  var blog = document.getElementById('blog');
+  var home = document.getElementById('home');
 
-var blogIsShowing = false;
-var toggleBlogSection = function() {
-  if (blogIsShowing) {
-    blog.classList.remove(SHOWING);
-    home.classList.add(SHOWING);
-  } else {
-    blog.classList.add(SHOWING);
-    home.classList.remove(SHOWING);
-  }
+  var blogIsShowing = false;
+  var toggleBlogSection = function() {
+    if (blogIsShowing) {
+      blog.classList.remove(SHOWING);
+      home.classList.add(SHOWING);
+    } else {
+      blog.classList.add(SHOWING);
+      home.classList.remove(SHOWING);
+    }
 
-  blogIsShowing = !blogIsShowing;
-};
-document.getElementById('link-from-home-to-blog').addEventListener('click', toggleBlogSection);
+    blogIsShowing = !blogIsShowing;
+  };
+  document.getElementById('link-from-home-to-blog').addEventListener('click', toggleBlogSection);
 
 
-var btnBackToPosts = document.getElementById('btn-back-to-post-links');
-var btnBackToHome = document.getElementById('btn-back-to-home');
+  var btnBackToPosts = document.getElementById('btn-back-to-post-links');
+  var btnBackToHome = document.getElementById('btn-back-to-home');
 
-var listOfPosts = document.getElementById('blog-post-links');
-var hideListOfPosts = function() { listOfPosts.style.display = 'none' };
-var showListOfPosts = function() { listOfPosts.style.display = 'block' };
+  var listOfPosts = document.getElementById('blog-post-links');
+  var hideListOfPosts = function() { listOfPosts.style.display = 'none' };
+  var showListOfPosts = function() { listOfPosts.style.display = 'block' };
 
-var shownPost = null;
-var hideTheBlogPost = function() { shownPost && shownPost.classList.remove(SHOWING) };
-var showBlogPost = function(postId) {
-  shownPost = document.getElementById(postId);
-  shownPost.classList.add(SHOWING);
-  hideListOfPosts();
-  btnBackToPosts.classList.add(SHOWING);
-};
+  var shownPost = null;
+  var hideTheBlogPost = function() { shownPost && shownPost.classList.remove(SHOWING) };
+  var showBlogPost = function(postId) {
+    shownPost = document.getElementById(postId);
+    shownPost.classList.add(SHOWING);
+    hideListOfPosts();
+    btnBackToPosts.classList.add(SHOWING);
+  };
 
-var backToPosts = function() {
-  hideTheBlogPost();
-  showListOfPosts();
-  btnBackToPosts.classList.remove(SHOWING);
-};
-btnBackToHome.addEventListener('click', function() {
-  backToPosts();
-  toggleBlogSection();
-});
-btnBackToPosts.addEventListener('click', backToPosts);
+  var backToPosts = function() {
+    hideTheBlogPost();
+    showListOfPosts();
+    btnBackToPosts.classList.remove(SHOWING);
+  };
+  btnBackToHome.addEventListener('click', function() {
+    backToPosts();
+    toggleBlogSection();
+  });
+  btnBackToPosts.addEventListener('click', backToPosts);
 
-// Initialize the blog post links.
-(function() {
-  var blogPostLinks = document.getElementsByClassName('blog-post-link');
-  for (var i = 0; i < blogPostLinks.length; i++) {
-    var link = blogPostLinks[i];
-    var postId = link.children[0].id.slice('link-for-'.length);
-    link.addEventListener('click', showBlogPost.bind(null, postId));
-  }
-})();
+  // Initialize the blog post links.
+  (function() {
+    var blogPostLinks = document.getElementsByClassName('blog-post-link');
+    for (var i = 0; i < blogPostLinks.length; i++) {
+      var link = blogPostLinks[i];
+      var postId = link.children[0].id.slice('link-for-'.length);
+      link.addEventListener('click', showBlogPost.bind(null, postId));
+    }
+  })();
 
 })();
