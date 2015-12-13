69b893e9d208af01dc87d871b64373ea4f0216b1
Brian Weidenbaum <brianweiden@gmail.com>
2015-12-12 18:03:53 -0800

    refactor to use classes

--- script.js
+++ script.js
@@ -1,59 +1,102 @@
 (function() {
   var SHOWING = 'showing';
 
-  var blog = document.getElementById('blog');
-  var home = document.getElementById('home');
+  var MainController = function(homeView, blogView) {
+    this.home_ = homeView;
+    this.blog_ = blogView;
 
-  var blogIsShowing = false;
-  var toggleBlogSection = function() {
-    if (blogIsShowing) {
-      blog.classList.remove(SHOWING);
-      home.classList.add(SHOWING);
+    this.blogIsShowing_ = blog.classList.contains(SHOWING);
+  };
+
+  /** Shows the blog if it's hiding; hides it if it's showing. */
+  MainController.prototype.showOrHideBlog = function() {
+    if (this.blogIsShowing_) {
+      this.blog_.classList.remove(SHOWING);
+      this.home_.classList.add(SHOWING);
     } else {
-      blog.classList.add(SHOWING);
-      home.classList.remove(SHOWING);
+      this.blog_.classList.add(SHOWING);
+      this.home_.classList.remove(SHOWING);
     }
 
-    blogIsShowing = !blogIsShowing;
-  };
-  document.getElementById('link-from-home-to-blog').addEventListener('click', toggleBlogSection);
-
-
-  var btnBackToPosts = document.getElementById('btn-back-to-post-links');
-  var btnBackToHome = document.getElementById('btn-back-to-home');
-
-  var listOfPosts = document.getElementById('blog-post-links');
-  var hideListOfPosts = function() { listOfPosts.style.display = 'none' };
-  var showListOfPosts = function() { listOfPosts.style.display = 'block' };
-
-  var shownPost = null;
-  var hideTheBlogPost = function() { shownPost && shownPost.classList.remove(SHOWING) };
-  var showBlogPost = function(postId) {
-    shownPost = document.getElementById(postId);
-    shownPost.classList.add(SHOWING);
-    hideListOfPosts();
-    btnBackToPosts.classList.add(SHOWING);
+    this.blogIsShowing_ = !this.blogIsShowing_;
   };
 
-  var backToPosts = function() {
-    hideTheBlogPost();
-    showListOfPosts();
-    btnBackToPosts.classList.remove(SHOWING);
-  };
-  btnBackToHome.addEventListener('click', function() {
-    backToPosts();
-    toggleBlogSection();
-  });
-  btnBackToPosts.addEventListener('click', backToPosts);
 
-  // Initialize the blog post links.
+  var BlogController = function(listOfPosts, backToPostsButton, backToHomeButton) {
+    this.listOfPosts_ = listOfPosts;
+    this.btnBackToPosts_ = backToPostsButton;
+    this.btnBackToHome_ = backToHomeButton;
+
+    this.shownPost_ = null;
+  };
+
+  BlogController.prototype.hideTheBlogPost = function() {
+    if (this.shownPost_) this.shownPost_.classList.remove(SHOWING);
+  };
+
+  BlogController.prototype.showBackToPostsButton_ = function() {
+    this.btnBackToPosts_.classList.add(SHOWING);
+  };
+
+  BlogController.prototype.hideBackToPostsButton_ = function() {
+    this.btnBackToPosts_.classList.remove(SHOWING);
+  };
+
+  BlogController.prototype.hideListOfPosts_ = function() {
+    this.listOfPosts_.style.display = 'none';
+  };
+
+  BlogController.prototype.showListOfPosts = function() {
+    this.listOfPosts_.style.display = 'block';
+  };
+
+  BlogController.prototype.showBlogPost = function(postId) {
+    this.shownPost_ = document.getElementById(postId);
+    this.shownPost_.classList.add(SHOWING);
+
+    this.hideListOfPosts_();
+    this.showBackToPostsButton_();
+  };
+
+  BlogController.prototype.goBackToPostsList = function() {
+    this.hideTheBlogPost();
+    this.showListOfPosts();
+    this.hideBackToPostsButton_();
+  };
+
+
   (function() {
+    var btnBackToHome = document.getElementById('btn-back-to-home');
+    var btnBackToPosts = document.getElementById('btn-back-to-post-links');
+
+    var mainCtrl = new MainController(
+        document.getElementById('home'),
+        document.getElementById('blog')
+    );
+
+    var blogCtrl = new BlogController(
+        document.getElementById('blog-post-links'),
+        btnBackToPosts,
+        btnBackToHome
+    );
+
+    document.getElementById('link-from-home-to-blog').addEventListener('click', function() {
+      mainCtrl.showOrHideBlog();
+    });
+
+    btnBackToHome.addEventListener('click', function() {
+      blogCtrl.goBackToPostsList();
+      mainCtrl.showOrHideBlog();
+    });
+    btnBackToPosts.addEventListener('click', function() {
+      blogCtrl.goBackToPostsList()
+    });
+
     var blogPostLinks = document.getElementsByClassName('blog-post-link');
     for (var i = 0; i < blogPostLinks.length; i++) {
       var link = blogPostLinks[i];
       var postId = link.children[0].id.slice('link-for-'.length);
-      link.addEventListener('click', showBlogPost.bind(null, postId));
+      link.addEventListener('click', blogCtrl.showBlogPost.bind(blogCtrl, postId));
     }
   })();
-
 })();
